a8f0219ffe08c4ce752624cc3aa5486a66d61cd0
Robert Mang <Robert.Mang@softgarden.de>
2024-01-10 23:01:22 +0100

    remove main method code

--- main.go
+++ main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/MorrisMorrison/gutils/gollections"
-)
-
 func main() {
-	valuesToCheck := []int{0, 1, 2, 3, 4, 5}
-	containsValue := gollections.Contains(valuesToCheck, 1)
-	fmt.Println(containsValue)
 }
